deccfb5ad3a0384366051d2de0e7fcc541354e09
Mhayk Whandson <hi@mhayk.com>
2023-09-15 23:58:14 +0100

    feat: create the route to return a resume of audio transcription using openai(chatGPT)

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import { prisma } from './lib/prisma'
 import { getAllPromptsRoutes } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
 app.register(getAllPromptsRoutes)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333
